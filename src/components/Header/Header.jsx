cd6de1775324841db1596a9f64ddd5ac80626471
almamuntmn <almamuncool88@gmail.com>
2025-08-19 22:01:13 +0600

    fourth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='flex items bg-center justify-between border-b-2 pb-4'>
             <h1 class="text-3xl font-bold">
-                Knowledge Cafe
+                Knowledge Cafe Site
             </h1>
             <img src={profile} alt="" />
         </div>
