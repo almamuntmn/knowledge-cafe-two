ac23ba3b9576cf7b4cacdcc5330b05c95ca008e9
almamuntmn <almamuncool88@gmail.com>
2025-08-19 21:55:26 +0600

    2nd commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import profile from '../../assets/images/profile.png'
+
+const Header = () => {
+    return (
+        <div className='flex items bg-center justify-between border-b-2 pb-4'>
+            <h1 class="text-3xl font-bold">
+                Knowledge Cafe
+            </h1>
+            <img src={profile} alt="" />
+        </div>
+    );
+};
+
+export default Header;
